1a54a28a8ac1dfa025ddf28e623e9603b4aecb4c
marcelloHUNcut <134726897+marcelloHUNcut@users.noreply.github.com>
2024-12-05 16:34:28 +0100

    Add files via upload

---
+++ visualization/visuals/visuals/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace visuals
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            car = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)car).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // car
+            // 
+            car.Image = (Image)resources.GetObject("car.Image");
+            car.Location = new Point(0, 500);
+            car.Name = "car";
+            car.Size = new Size(80, 40);
+            car.SizeMode = PictureBoxSizeMode.StretchImage;
+            car.TabIndex = 1;
+            car.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(544, 1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(800, 800);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1869, 677);
+            Controls.Add(car);
+            Controls.Add(pictureBox2);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)car).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox car;
+        public System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox2;
+    }
+}
